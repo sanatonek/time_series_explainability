10c2d057e62b11d7bdc0ef58655866d9d01d91d9
sanatonek <sana.tonekaboni@mail.utoronto.ca>
2019-10-09 11:12:24 -0400

    Plot baseline importance results

--- TSX/generator_baselines.py
+++ TSX/generator_baselines.py
@@ -23,8 +23,7 @@ feature_map_mimic = ['ANION GAP', 'ALBUMIN', 'BICARBONATE', 'BILIRUBIN', 'CREATI
                      'Glucose', 'Temp']
 
 MIMIC_TEST_SAMPLES = [4387, 481, 546, 10]
-SIMULATION_SAMPLES = [101, 48]  # , 88, 192, 143, 166, 18, 58, 172, 132]
-# SIMULATION_SAMPLES = []
+SIMULATION_SAMPLES = [101, 48]
 samples_to_analyze = {'mimic': MIMIC_TEST_SAMPLES, 'simulation': SIMULATION_SAMPLES, 'ghg': [], 'simulation_spike': []}
 
 
@@ -64,13 +63,36 @@ def main(data, generator_type):
                                     historical=(configs['historical'] == 1), generator_type=generator_type, data=data,
                                     experiment=experiment + '_' + generator_type, spike_data=spike_data)
 
-    exp.run(train=False, n_epochs=configs['n_epochs'], samples_to_analyze=samples_to_analyze[data])
+    exp.run(train=False, n_epochs=configs['n_epochs'], samples_to_analyze=[1,2])
 
     testset = list(exp.test_loader.dataset)
     test_signals = torch.stack(([x[0] for x in testset])).to(device)
+
+    true_generator = TrueFeatureGenerator()
+    ffc_mse = 0
+    afo_mse = 0
+    fo_mse = 0
+    n = 200*test_signals.shape[1]*(test_signals.shape[-1]-1)
+    
+    for s in range(200):
+        signal = test_signals[s]
+        for t in range(1, test_signals.shape[-1]):
+            for f in range(test_signals.shape[1]):
+                feature_dist = (np.array(exp.feature_dist[:, f, :]).reshape(-1))
+                ffc_sample, _ = exp.generator(signal[: , t], signal[:, 0:t].view(1, signal.size(0), t), f, method='m1')
+                ffc_sample = ffc_sample.cpu().detach().numpy()
+                fo_sample = torch.Tensor(np.array([np.random.uniform(-3, 3)]).reshape(-1)).detach().numpy()
+                afo_sample = torch.Tensor(np.array(np.random.choice(feature_dist)).reshape(-1, )).detach().numpy()
+        true_sample = true_generator.sample(signal.cpu().numpy(), t, f)
+        # true_sample = np.float(signal[f,t])
+        ffc_mse = ffc_mse + np.mean(np.square(ffc_sample-true_sample))
+        afo_mse = afo_mse + np.mean(np.square(afo_sample-true_sample))
+        fo_mse = fo_mse + np.mean(np.square(fo_sample - true_sample))
+    print(ffc_mse/n, afo_mse/n, fo_mse/n)
+
     for sample_ID in samples_to_analyze[data]:
-        result_path = '/scratch/gobi1/sana/TSX_results/' + str(data) + '/results_%s.pkl' % str(sample_ID)
-        print('Reading results from ', result_path)
+        # result_path = '/scratch/gobi1/sana/TSX_results/' + str(data) + '/results_%s.pkl' % str(sample_ID)
+        result_path = '/scratch/gobi1/shalmali/' + str(data) + '/results_%s.pkl' % str(sample_ID)
         with open(result_path, 'rb') as f:
             arr = pkl.load(f)
 
@@ -78,28 +100,29 @@ def main(data, generator_type):
         fcc_importance = arr['FFC']['imp']
         afo_importance = arr['AFO']['imp']
         fo_importance = arr['Suresh_et_al']['imp']
+
         baseline_importance = find_true_gen_importance(test_signal, data,sample_ID)
 
         f, (ax1, ax2, ax3, ax4) = plt.subplots(4)
         for ft in range(test_signal.shape[0]):
             ax2.plot(fcc_importance[ft, :], linestyle='--', marker='o', markersize=10, label='feature %d' % ft)
-            ax2.set_title('FFC', fontweight='bold', fontsize=34)
+            ax2.set_title('FFC', fontweight='bold', fontsize=38)
             ax3.plot(afo_importance[ft, :], linestyle='--', marker='o', markersize=10, label='feature %d' % ft)
-            ax3.set_title('AFO', fontweight='bold', fontsize=34)
+            ax3.set_title('AFO', fontweight='bold', fontsize=38)
             ax4.plot(fo_importance[ft, :], linestyle='--', marker='o', markersize=10, label='feature %d' % ft)
-            ax4.set_title('Suresh et al', fontweight='bold', fontsize=34)
+            ax4.set_title('FO', fontweight='bold', fontsize=38)
             ax1.plot(baseline_importance[ft, :], linestyle='--', marker='o', markersize=10, label='feature %d' % ft)
             ax1.set_title('True Generator', fontweight='bold', fontsize=34)
-            ax1.set_ylabel('importance score', fontweight='bold', fontsize=18)
-            ax2.set_ylabel('importance score', fontweight='bold', fontsize=18)
-            ax3.set_ylabel('importance score', fontweight='bold', fontsize=18)
-            ax4.set_ylabel('importance score', fontweight='bold', fontsize=18)
-            ax1.tick_params(axis='both', labelsize=26)
-            ax2.tick_params(axis='both', labelsize=26)
-            ax3.tick_params(axis='both', labelsize=26)
-            ax4.tick_params(axis='both', labelsize=26)
-            ax4.set_xlabel('time', fontweight='bold', fontsize=24)
-        f.set_figheight(25)
+            ax1.set_ylabel('importance score', fontweight='bold', fontsize=28)
+            ax2.set_ylabel('importance score', fontweight='bold', fontsize=28)
+            ax3.set_ylabel('importance score', fontweight='bold', fontsize=28)
+            ax4.set_ylabel('importance score', fontweight='bold', fontsize=28)
+            ax1.tick_params(axis='both', labelsize=32)
+            ax2.tick_params(axis='both', labelsize=32)
+            ax3.tick_params(axis='both', labelsize=32)
+            ax4.tick_params(axis='both', labelsize=32)
+            ax4.set_xlabel('time', fontweight='bold', fontsize=32)
+        f.set_figheight(20)
         f.set_figwidth(30)
         plt.savefig(os.path.join('/scratch/gobi1/sana/TSX_results', data, 'generator_baselines_%d.pdf' % (sample_ID)),
                     dpi=300, orientation='landscape', bbox_inches='tight')
@@ -131,15 +154,15 @@ def find_true_gen_importance(sample, data, sampleID):
                 torch.cat((sample[:, :t], full_counterfact.view(-1, 1)), -1).view(1, n_feature, -1))
             importance[f, t - 1] = abs(true_risk[t - 1] - predicted_risk.item())
 
-    with open(os.path.join('/scratch/gobi1/shalmali', data, '/results_' + str(sampleID) + '.pkl'), 'wb') as f:
-        pkl.dump(importance)
+   # with open(os.path.join('/scratch/gobi1/shalmali', data, '/results_' + str(sampleID) + '.pkl'), 'wb') as f:
+    #    pkl.dump(importance)
 
     return (importance)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train an ICU mortality prediction model')
-    parser.add_argument('--data', type=str, default='mimic')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, default='simulation')
     parser.add_argument('--generator', type=str, default='joint_RNN_generator')
     args = parser.parse_args()
     main(data=args.data, generator_type=args.generator)
