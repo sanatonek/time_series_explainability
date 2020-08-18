57b67b210ac50e15894890b21ffe61a9b3dd7f0f
sanatonek <sana.tonekaboni@mail.utoronto.ca>
2020-08-18 11:13:58 -0400

    Added carry forward and mean imputation + Code clean up

--- evaluation/baselines.py
+++ evaluation/baselines.py
@@ -3,20 +3,17 @@ import argparse
 
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
 import seaborn as sns; sns.set()
 import pickle as pkl
 import time
-import pandas as pd
-from scipy import interpolate
 
-from TSX.utils import load_simulated_data, train_model_rt, shade_state, shade_state_state_data, compute_median_rank,\
-    plot_heatmap_text, train_model_rt_binary, train_model, load_data
-from TSX.models import StateClassifier, RETAIN, EncoderRNN #, TrueClassifier
+from TSX.utils import load_simulated_data, train_model_rt, compute_median_rank,\
+    train_model_rt_binary, train_model, load_data
+from TSX.models import StateClassifier, RETAIN, EncoderRNN
 from TSX.generator import JointFeatureGenerator, JointDistributionGenerator
 from TSX.explainers import RETAINexplainer, FITExplainer, IGExplainer, FFCExplainer, \
-    DeepLiftExplainer, GradientShapExplainer, AFOExplainer, FOExplainer, SHAPExplainer, LIMExplainer
+    DeepLiftExplainer, GradientShapExplainer, AFOExplainer, FOExplainer, SHAPExplainer, \
+    LIMExplainer, CarryForwardExplainer, MeanImpExplainer
 from sklearn import metrics
 
 intervention_list = ['vent', 'vaso', 'adenosine', 'dobutamine', 'dopamine', 'epinephrine', 'isuprel', 'milrinone',
@@ -28,7 +25,6 @@ feature_map_mimic = ['ANION GAP', 'ALBUMIN', 'BICARBONATE', 'BILIRUBIN', 'CREATI
 
 ks = {'simulation_spike': 1, 'simulation': 3, 'simulation_l2x': 4}
 
-# from captum.attr import IntegratedGradients, DeepLift, GradientShap, Saliency
 
 if __name__ == '__main__':
     np.random.seed(1234)
@@ -141,6 +137,12 @@ if __name__ == '__main__':
         elif args.explainer == 'afo':
             explainer = AFOExplainer(model, train_loader)
 
+        elif args.explainer == 'carry_forward':
+            explainer = CarryForwardExplainer(model, train_loader)
+
+        elif args.explainer == 'mean_imp':
+            explainer = MeanImpExplainer(model, train_loader)
+
         elif args.explainer == 'gradient_shap':
             explainer = GradientShapExplainer(model)
 
@@ -159,11 +161,20 @@ if __name__ == '__main__':
         elif args.explainer == 'lime':
             explainer = LIMExplainer(model, train_loader)
 
-        # elif args.explainer == 'dist':
-        #     explainer = DistGenerator(model, train_loader, n_componenets=2)
-
         else:
             raise ValueError('%s explainer not defined!' % args.explainer)
+
+    # Load ground truth for simulations
+    if data_type == 'state':
+        with open(os.path.join(data_path, 'state_dataset_importance_test.pkl'), 'rb') as f:
+            gt_importance_test = pkl.load(f)
+        with open(os.path.join(data_path, 'state_dataset_states_test.pkl'), 'rb') as f:
+            state_test = pkl.load(f)
+        with open(os.path.join(data_path, 'state_dataset_logits_test.pkl'), 'rb') as f:
+            logits_test = pkl.load(f)
+    elif data_type == 'spike':
+        with open(os.path.join(data_path, 'gt_test.pkl'), 'rb') as f:
+            gt_importance_test = pkl.load(f)
 
     importance_scores = []
     ranked_feats=[]
@@ -174,146 +185,28 @@ if __name__ == '__main__':
         x = x.to(device)
         y = y.to(device)
 
-        if data_type == 'state':
-            with open(os.path.join(data_path, 'state_dataset_importance_test.pkl'), 'rb') as f:
-                gt_importance_test = pkl.load(f)
-            with open(os.path.join(data_path, 'state_dataset_states_test.pkl'),'rb') as f:
-                state_test = pkl.load(f)
-            with open(os.path.join(data_path, 'state_dataset_logits_test.pkl'),'rb') as f:
-                logits_test = pkl.load(f)
-        elif data_type == 'spike':
-            with open(os.path.join(data_path, 'gt_test.pkl'), 'rb') as f:
-                gt_importance_test = pkl.load(f)
-        else:
-            pass
-
+        t0 = time.time()
         score = explainer.attribute(x, y if args.data=='mimic' else y[:, -1].long())
-
         ranked_features = np.array([((-(score[n])).argsort(0).argsort(0) + 1) \
                                     for n in range(x.shape[0])])  # [:ks[args.data]]
-        # if args.explainer == 'fit':
-        #     score_mean_shift = explainer.attribute(x, y[:, -1].long(), distance_metric='mean_divergence')
         importance_scores.append(score)
         ranked_feats.append(ranked_features)
 
-        importance_scores = np.concatenate(importance_scores, 0)
-        print('*******************\n Saving file to ',
-              os.path.join(output_path, '%s_test_importance_scores_%d.pkl' % (args.explainer, args.cv)))
-        print(output_path)
-        with open(os.path.join(output_path, '%s_test_importance_scores_%d.pkl' % (args.explainer, args.cv)), 'wb') as f:
-            pkl.dump(importance_scores, f, protocol=pkl.HIGHEST_PROTOCOL)
+    importance_scores = np.concatenate(importance_scores, 0)
+    print('Saving file to ', os.path.join(output_path, '%s_test_importance_scores_%d.pkl' % (args.explainer, args.cv)))
+    with open(os.path.join(output_path, '%s_test_importance_scores_%d.pkl' % (args.explainer, args.cv)), 'wb') as f:
+        pkl.dump(importance_scores, f, protocol=pkl.HIGHEST_PROTOCOL)
 
-        labels = np.zeros((x.shape[0], x.shape[-1]))
-        for t in range(1, x.shape[-1]):
-            if args.explainer=='retain':
-                x_retain = x.permute(0, 2, 1)
-                p_y_t, _, _ = explainer.base_model(x_retain[:, :t + 1, :], (torch.ones((len(x),)) * t+1))
-            else:
-                p_y_t = explainer.base_model(x[:, :, :t + 1], return_multi=True)
-            labels[:, t - 1] = np.array([p > 0.5 for p in p_y_t.cpu().detach().numpy()[:, 1]]).flatten()
-        #print(np.any(np.isnan(gt_importance_test)), np.any(np.isnan(ranked_features)), np.any(np.isnan(score)))
-        break
-
-
-    # Print results
-    plot_id = 7
-
-    f, axs = plt.subplots(3)
-    f.set_figheight(6)
-    f.set_figwidth(10)
-    score_pd = pd.DataFrame(columns=['f1', 'f2', 'f3', 's1', 's2', 's3'])
-    score_pd['t'] = pd.Series(np.arange(1, score[plot_id].shape[-1]))
-    cmap = sns.cubehelix_palette(rot=.2, as_cmap=True)
-    bottom = cm.get_cmap('Blues', 128)
-    for feat in [1, 2, 3]:  # range(1,2):
-        score_pd['f%d' % feat] = pd.Series(x[plot_id, feat - 1, 1:].cpu().numpy())
-        score_pd['s%d' % feat] = pd.Series(score[plot_id, feat - 1, :])
-        f = interpolate.interp1d(score_pd['t'], score_pd['f%d' % feat], fill_value="extrapolate")
-        f_score = interpolate.interp1d(score_pd['t'], score_pd['s%d' % feat], fill_value="extrapolate")
-        xnew = np.arange(1, score[plot_id].shape[-1] - 0.99, 0.01)
-        ynew = f(xnew)
-        score_new = f_score(xnew)
-        # axs[feat-1].scatter(xnew, ynew, c=cm.hot(score_new/2.+0.5), edgecolor='none')
-        axs[feat - 1].scatter(xnew, ynew, c=bottom(score_new / 2. + 0.5), edgecolor='none')
-    plt.legend()
-    plt.savefig(os.path.join(plot_path, 'new_viz.pdf'), dpi=300, orientation='landscape')
-
-    t_len = score[plot_id].shape[-1]
-    f, axs = plt.subplots(3)
-
-    plot_heatmap_text(ranked_features[plot_id, :, 1:], score[plot_id, :, 1:],
-                      os.path.join(plot_path, '%s_example_heatmap.pdf' % args.explainer), axs[2])
-    t = np.arange(1, t_len)
-
-    # pred_batch_vec = []
-    # model.eval()
-    # for tt in t:
-    #     pred_tt = model(x[plot_id, :, :tt + 1].unsqueeze(0)).detach().cpu().numpy()
-    #     # pred_tt = np.argmax(pred_tt, -1)
-    #     pred_tt = pred_tt[:,-1]
-    #     pred_batch_vec.append(pred_tt)
-
-
-    if data_type == 'state':
-
-        shade_state_state_data(state_test[plot_id], t, axs[0])
-        '''
-        if args.gt == 'pred_model':
-            for tt in t:
-                if tt>1:
-                    label_change = abs((pred_batch_vec[tt-1][:,1]-pred_batch_vec[tt-2][:,1]).reshape(-1,1))
-                    gt_importance_test[:,:,tt-1] = np.multiply(np.repeat(label_change,x.shape[1],axis=1), \
-                    gt_importance_test[:,:,tt-1])
-        elif args.gt == 'true_model':
-            for tt in t:
-                if tt>1:
-                    label_change = abs((y[:,tt-1]-y[:,tt-2]).cpu().detach().numpy().reshape(-1,1))
-                    gt_importance_test[:,:,tt-1] = np.multiply(np.repeat(label_change,x.shape[1],axis=1), \
-                    gt_importance_test[:,:,tt-1])
-
-                    logits_change = abs((logits_test[:,tt-1]-logits_test[:,tt-2]).reshape(-1,1))
-                    gt_soft_score[:,:,tt-1] = np.multiply(np.repeat(logits_change,x.shape[1],axis=1), \
-                    gt_importance_test[:,:,tt-1])
-        '''
-    for i, ref_ind in enumerate(range(x[plot_id].shape[0])):
-        axs[0].plot(t, x[plot_id, ref_ind, 1:].cpu().numpy(), linewidth=3, label='feature %d' % (i))
-        axs[1].plot(t, score[plot_id, ref_ind, 1:], linewidth=3, label='importance %d' % (i))
-        # if args.explainer == 'fit':
-        #     axs[3].plot(t,
-        #                 score_mean_shift[plot_id, ref_ind, 1:], linewidth=3, label='importance %d' % (i))
-
-    # axs[0].plot(t, pred_batch_vec, '--', linewidth=3, c='black')
-    # axs[0].plot(t, y[plot_id, 1:].cpu().numpy(), '--', linewidth=3, c='red')
-    axs[0].tick_params(axis='both', labelsize=20)
-    axs[2].tick_params(axis='both', labelsize=20)
-    axs[1].tick_params(axis='both', labelsize=20)
-    axs[1].set_title('%s'%args.explainer, fontsize=30)
-    axs[0].margins(0.03)
-    axs[2].margins(0.03)
-    axs[1].margins(0.03)
-
-    # axs[0].grid()
-    f.set_figheight(16)
-    f.set_figwidth(24)
-    plt.subplots_adjust(hspace=.5)
-    name = args.explainer + '_' + args.generator_type if args.explainer == 'fit' else args.explainer
-    plt.savefig(os.path.join(plot_path, '%s_example.pdf' % name), dpi=300, orientation='landscape')
-    fig_legend = plt.figure(figsize=(13, 1.2))
-    handles, labels = axs[0].get_legend_handles_labels()
-    plt.figlegend(handles, labels, loc='upper left', ncol=4, fancybox=True, handlelength=6, fontsize='xx-large')
-    fig_legend.savefig(os.path.join(plot_path, '%s_example_legend.pdf' % name), dpi=300, bbox_inches='tight')
-
-    # importance_scores = np.concatenate(importance_scores, 0)
     ranked_feats = np.concatenate(ranked_feats,0)
     with open(os.path.join(output_path, '%s_test_ranked_scores.pkl' % args.explainer), 'wb') as f:
-        pkl.dump(ranked_feats,f,protocol=pkl.HIGHEST_PROTOCOL)
+        pkl.dump(ranked_feats, f, protocol=pkl.HIGHEST_PROTOCOL)
 
     if 'simulation' in args.data:
         gt_soft_score = np.zeros(gt_importance_test.shape)
         gt_importance_test.astype(int)
         gt_score = gt_importance_test.flatten()
         explainer_score = importance_scores.flatten()
-        if (args.explainer=='deep_lift' or args.explainer=='integrated_gradient' or args.explainer=='gradient_shap'):
+        if args.explainer=='deep_lift' or args.explainer=='integrated_gradient' or args.explainer=='gradient_shap':
             explainer_score = np.abs(explainer_score)
         auc_score = metrics.roc_auc_score(gt_score, explainer_score)
         aupr_score = metrics.average_precision_score(gt_score, explainer_score)
